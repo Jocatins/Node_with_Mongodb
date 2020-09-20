828c0e24323d60d5c40a1970055d298a2872175e
Jocatins <jocakhamen@gmail.com>
2020-09-20 11:20:18 +0100

    Node with Mongodb

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const PORT = process.env.PORT || 7050;
+const express = require('express');
+
+const app = express();
+
+const appRoutes = require('./routes/appRoutes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/meanDb', { useUnifiedTopology: true });
+mongoose.connection
+  .once('open', function () {
+    console.log('Mongoose Connected!!!');
+  })
+  .on('error', function (error) {
+    console.log('Connection error', error);
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', appRoutes);
+
+http.createServer(app).listen(PORT);
+
+console.log('Port is running on:', PORT);
